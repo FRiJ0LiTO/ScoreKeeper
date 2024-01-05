adb93b1f2f9aa65d9904abc3d9bcc9cf39d85a3c
FRiJ0LITO <a00835376@tec.mx>
2024-01-05 12:11:38 -0600

    ScoreKeeper Refactoring

--- app.js
+++ app.js
@@ -1,63 +1,56 @@
+const player1 = {
+    score: 0,
+    button: document.querySelector('#player-1-btn'),
+    display: document.querySelector('#player-1')
+}
+
+const player2 = {
+    score: 0,
+    button: document.querySelector('#player-2-btn'),
+    display: document.querySelector('#player-2')
+}
+
 // Opciones de puntuación máxima para el juego
 const options = document.querySelector('#options-match');
-
-// Marcadores de los jugadores (Tabla de puntuaciones)
-const firstScore = document.querySelector('#player-1');
-const secondScore = document.querySelector('#player-2');
-
-// Botones de los Jugadores
-const firstPlayer = document.querySelector('#player-1-btn');
-const secondPlayer = document.querySelector('#player-2-btn');
 
 // Botón de reset
 const resetScore = document.querySelector('#reset-btn')
 
-let scoreOne = parseInt(firstScore.innerText);
-let scoreTwo = parseInt(secondScore.innerText);
 let isGameOver = false;
-let winningScore = 5;
+let winningScore = 3;
+
+const updateScore = (player, opponent) => {
+    if (!isGameOver) {
+        player.score++;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.innerText = player.score;
+    }
+
+}
 
 const reset = () => {
     isGameOver = false;
-    scoreOne = 0;
-    scoreTwo = 0;
-    firstScore.textContent = 0;
-    secondScore.textContent = 0;
-    // Reseteamos las clases de los marcadores para que no se queden en rojo ni verde
-    firstScore.classList.remove('has-text-success', 'has-text-danger');
-    secondScore.classList.remove('has-text-success', 'has-text-danger');
-    firstPlayer.disabled = false;
-    secondPlayer.disabled = false;
+    for (let p of [player1, player2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        // Reseteamos las clases de los marcadores para que no se queden en rojo ni verde
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
 };
 
-firstPlayer.addEventListener('click', () => {
-    if (!isGameOver) {
-        scoreOne++;
-        if (scoreOne === winningScore) {
-            isGameOver = true;
-            // Añadimos las clases de CSS para que se pongan en rojo y verde
-            firstScore.classList.add('has-text-success');
-            secondScore.classList.add('has-text-danger');
-            // Deshabilitamos los botones para que no se pueda seguir sumando puntos
-            firstPlayer.disabled = true;
-            secondPlayer.disabled = true;
-        }
-        firstScore.innerText = scoreOne;
-    }
+player1.button.addEventListener('click', function () {
+    updateScore(player1, player2)
 })
 
-secondPlayer.addEventListener('click', () => {
-    if (!isGameOver) {
-        scoreTwo++;
-        if (scoreTwo === winningScore) {
-            isGameOver = true;
-            secondScore.classList.add('has-text-success');
-            firstScore.classList.add('has-text-danger');
-            firstPlayer.disabled = true;
-            secondPlayer.disabled = true;
-        }
-        secondScore.innerText = scoreTwo;
-    }
+player2.button.addEventListener('click', function () {
+    updateScore(player2, player1)
 })
 
 options.addEventListener('change', () => {
@@ -67,5 +60,7 @@ options.addEventListener('change', () => {
     reset();
 })
 
-resetScore.addEventListener('click', reset)
+resetScore.addEventListener('click', function () {
+    reset();
+})
 
